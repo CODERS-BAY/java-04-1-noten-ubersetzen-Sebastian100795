685734b19c2e63bf107be65b29dd8c2db6c73ad8
Sebastian Pichler <s.pichler100795@gmail.com>
2021-02-15 11:12:44 +0100

    add exercise

--- src/src/main.java
+++ src/src/main.java
@@ -8,22 +8,14 @@ public class main {
         switch (note) {
             case 1:
                 System.out.println("Sehr Gut");
-                break;
             case 2:
                 System.out.println("Gut");
-                break;
             case 3:
                 System.out.println("Befriedigend");
-                break;
             case 4:
                 System.out.println("Genügend");
-                break;
             case 5:
                 System.out.println("Nicht Genügend");
-                break;
-            default:
-                System.out.println("Das ist keine Gültige eingabe.");
-                break;
         }
     }
 }
